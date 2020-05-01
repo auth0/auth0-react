e55296d15a215bfef854a035b87540f4e217ac15
adamjmcgrath <adam.mcgrath@auth0.com>
2020-05-01 11:59:09 +0100

    Add junit reporter

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
   clearMocks: true,
   coverageReporters: ['lcov', 'text', 'text-summary'],
   preset: 'ts-jest',
+  reporters: [
+    'default',
+    ['jest-junit', { outputDirectory: 'test-results/jest' }]
+  ],
   testRegex: '/__tests__/.+test\.tsx?$'
 };
