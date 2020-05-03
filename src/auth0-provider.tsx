245b5dd4ec6f7a376de4bc8d8171f3eae4a5fecf
adamjmcgrath <adam.mcgrath@auth0.com>
2020-05-03 12:41:05 +0100

    Add eslint

--- src/auth0-provider.tsx
+++ src/auth0-provider.tsx
@@ -3,13 +3,15 @@ import { Auth0Client, Auth0ClientOptions } from '@auth0/auth0-spa-js';
 
 export const Auth0Context = createContext<Auth0Client | null>(null);
 
-export default ({
+const Auth0Provider = ({
   children,
   ...opts
-}: PropsWithChildren<Auth0ClientOptions>) => {
+}: PropsWithChildren<Auth0ClientOptions>): JSX.Element => {
   const client = new Auth0Client(opts);
 
   return (
     <Auth0Context.Provider value={client}>{children}</Auth0Context.Provider>
   );
 };
+
+export default Auth0Provider;
