b7094d07271592971c817b989b0ddf43b8189615
Adam Mcgrath <adam.mcgrath@auth0.com>
2020-05-04 18:39:10 +0100

    [SDK-1576] Linting (#4)
    * Add prettier
    * Add eslint

--- src/auth0-provider.tsx
+++ src/auth0-provider.tsx
@@ -1,14 +1,17 @@
 import React, { createContext, PropsWithChildren } from 'react';
 import { Auth0Client, Auth0ClientOptions } from '@auth0/auth0-spa-js';
 
-export const Auth0Context = createContext<Auth0Client|null>(null);
+export const Auth0Context = createContext<Auth0Client | null>(null);
 
-export default ({ children, ...opts }: PropsWithChildren<Auth0ClientOptions>) => {
+const Auth0Provider = ({
+  children,
+  ...opts
+}: PropsWithChildren<Auth0ClientOptions>): JSX.Element => {
   const client = new Auth0Client(opts);
 
   return (
-    <Auth0Context.Provider value={client}>
-      {children}
-    </Auth0Context.Provider>
+    <Auth0Context.Provider value={client}>{children}</Auth0Context.Provider>
   );
 };
+
+export default Auth0Provider;
