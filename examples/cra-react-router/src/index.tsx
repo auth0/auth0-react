a2dbc39a0496b43830c9204394f92019c23fe39e
Adam Mcgrath <adam.mcgrath@auth0.com>
2020-09-25 09:22:02 +0100

    Export AppState type for custom onredirectCallbacks's (#120)

--- examples/cra-react-router/src/index.tsx
+++ examples/cra-react-router/src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App, { history } from './App';
-import { Auth0Provider } from '@auth0/auth0-react';
+import { Auth0Provider, AppState } from '@auth0/auth0-react';
 
-const onRedirectCallback = (appState: any) => {
+const onRedirectCallback = (appState: AppState) => {
   // If using a Hash Router, you need to use window.history.replaceState to
   // remove the `code` and `state` query parameters from the callback url.
   // window.history.replaceState({}, document.title, window.location.pathname);
