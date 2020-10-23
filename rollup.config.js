90d5c7a6995eeab077a982dea6196f9f6743de72
Frederik Prijck <frederik.prijck@auth0.com>
2020-10-23 09:09:02 +0200

    Add bundle analysis output when running build (#139)

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,7 @@ import { terser } from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import pkg from './package.json';
+import analyze from 'rollup-plugin-analyzer';
 
 const isProduction = process.env.NODE_ENV === 'production';
 const name = 'reactAuth0';
@@ -21,6 +22,7 @@ const plugins = [
   external(),
   resolve(),
   replace({ __VERSION__: `'${pkg.version}'` }),
+  analyze({ summaryOnly: true }),
 ];
 
 export default [
