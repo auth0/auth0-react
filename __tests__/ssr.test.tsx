2ca701ed78f68d60c5a6b9e36127ba53249bb873
Adam Mcgrath <adam.mcgrath@auth0.com>
2021-02-05 12:06:14 +0000

    isLoading should default to true even when doing SSR (#193)

--- __tests__/ssr.test.tsx
+++ __tests__/ssr.test.tsx
@@ -1,21 +1,26 @@
 /**
  * @jest-environment node
  */
-import { renderHook } from '@testing-library/react-hooks';
-import { createWrapper } from './helpers';
-import { useAuth0 } from '../src';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { Auth0Provider, Auth0Context } from '../src';
 
 jest.unmock('@auth0/auth0-spa-js');
 
 describe('In a Node SSR environment', () => {
   it('auth state is initialised', async () => {
-    const wrapper = createWrapper();
-    const {
-      result: {
-        current: { isLoading, isAuthenticated, user, loginWithRedirect },
-      },
-    } = renderHook(useAuth0, { wrapper });
-    expect(isLoading).toBeFalsy();
+    let isLoading, isAuthenticated, user, loginWithRedirect;
+    ReactDOMServer.renderToString(
+      <Auth0Provider clientId="__client_id__" domain="__domain__">
+        <Auth0Context.Consumer>
+          {(value): JSX.Element => {
+            ({ isLoading, isAuthenticated, user, loginWithRedirect } = value);
+            return <div>App</div>;
+          }}
+        </Auth0Context.Consumer>
+      </Auth0Provider>
+    );
+    expect(isLoading).toBeTruthy();
     expect(isAuthenticated).toBeFalsy();
     expect(user).toBeUndefined();
     await expect(loginWithRedirect).rejects.toThrowError(
