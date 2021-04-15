1aefc1dacd4f4813ceb18ea4f59d19bb9a877286
James Monger <jameskmonger@hotmail.co.uk>
2021-04-15 12:11:55 +0100

    add to cra example

--- examples/cra-react-router/src/Nav.tsx
+++ examples/cra-react-router/src/Nav.tsx
@@ -3,7 +3,9 @@ import { useHistory, Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
 export function Nav() {
-  const { isAuthenticated, user, loginWithRedirect, logout } = useAuth0();
+  const { isAuthenticated, user, loginWithRedirect, logout } = useAuth0<{
+    name: string;
+  }>();
   const history = useHistory();
   const [pathname, setPathname] = useState(() => history.location.pathname);
 
