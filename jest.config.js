3a8a3d753611a380c546b13fe7e7847b88062377
Marcin <marcin.krysiak@gmail.com>
2021-05-11 19:56:59 +0100

    disable coverage treshold

--- jest.config.js
+++ jest.config.js
@@ -19,14 +19,6 @@ module.exports = {
     },
     __VERSION__: pkg.version,
   },
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
-    },
-  },
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
     '^.+\\.(js|jsx)$': 'babel-jest',
