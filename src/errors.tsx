cab0248a4ac01f4288429d11745d358be551749c
Michael Copland <mjbcopland@gmail.com>
2022-06-03 16:32:54 +0100

    Correctly extend Error for OAuthError

--- src/errors.tsx
+++ src/errors.tsx
@@ -7,5 +7,8 @@
 export class OAuthError extends Error {
   constructor(public error: string, public error_description?: string) {
     super(error_description || error);
+
+    // https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work
+    Object.setPrototypeOf(this, OAuthError.prototype);
   }
 }
