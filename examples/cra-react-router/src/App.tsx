809922bf32384ab6a98b2898139d38fe7025ca3b
Adam Mcgrath <adam.mcgrath@auth0.com>
2022-07-21 14:15:37 +0100

    Move withAuthenticationRequired HOC out of render (#385)

--- examples/cra-react-router/src/App.tsx
+++ examples/cra-react-router/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
+import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Nav } from './Nav';
 import { Error } from './Error';
 import { Loading } from './Loading';
 import { Users } from './Users';
-import { ProtectedRoute } from './ProtectedRoute';
+
+const ProtectedUsers = withAuthenticationRequired(Users);
 
 function App() {
   const { isLoading, error } = useAuth0();
@@ -21,7 +22,7 @@ function App() {
       {error && <Error message={error.message} />}
       <Routes>
         <Route path="/" />
-        <Route path="/users" element={<ProtectedRoute component={Users} />} />
+        <Route path="/users" element={<ProtectedUsers />} />
       </Routes>
     </>
   );
