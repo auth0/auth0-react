716b96cfae36bda93d1d38427c61fcd73d332152
Adam Mcgrath <adam.mcgrath@auth0.com>
2022-07-26 12:44:11 +0100

    AuthState is public API

--- typedoc.js
+++ typedoc.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: '@auth0/auth0-react',
   out: './docs/',
-  exclude: ['./src/utils.tsx', './src/reducer.tsx', './src/auth-state.tsx'],
+  exclude: ['./src/utils.tsx', './src/reducer.tsx'],
   excludeExternals: false,
   excludePrivate: true,
   hideGenerator: true,
