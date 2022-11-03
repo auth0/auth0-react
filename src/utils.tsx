d6db49c6d016d6c86b62c6bb1b57f29efff1d158
Tommy Svensson <4046409+krokben@users.noreply.github.com>
2022-11-03 09:12:21 +0100

    fix error type in normalizeErrorFn

--- src/utils.tsx
+++ src/utils.tsx
@@ -8,17 +8,17 @@ export const hasAuthParams = (searchParams = window.location.search): boolean =>
   (CODE_RE.test(searchParams) || ERROR_RE.test(searchParams)) &&
   STATE_RE.test(searchParams);
 
-const normalizeErrorFn = (fallbackMessage: string) => (
-  error: Error | { error: string; error_description?: string } | ProgressEvent
-): Error => {
-  if ('error' in error) {
-    return new OAuthError(error.error, error.error_description);
-  }
-  if (error instanceof Error) {
-    return error;
-  }
-  return new Error(fallbackMessage);
-};
+const normalizeErrorFn =
+  (fallbackMessage: string) =>
+  (error: any | unknown): Error => {
+    if ('error' in error) {
+      return new OAuthError(error.error, error.error_description);
+    }
+    if (error instanceof Error) {
+      return error;
+    }
+    return new Error(fallbackMessage);
+  };
 
 export const loginError = normalizeErrorFn('Login failed');
 
