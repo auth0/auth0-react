7768f517653af90c788c151daa3cdb284247fd92
Ewan Harris <ewan.harris@okta.com>
2022-12-07 20:53:56 +0000

    Improve check of when users page has fully loaded in integration tests (#452)

--- cypress/integration/smoke.test.ts
+++ cypress/integration/smoke.test.ts
@@ -31,8 +31,10 @@ describe('Smoke tests', () => {
 
     loginToAuth0();
 
+    // Make sure the table has rendered with data as that is when the page has loaded completely
+    // and there shouldn't be any issues with the logout button being recreated
+    cy.get('table tbody tr').should('have.length', 2);
     cy.url().should('include', '/users');
-    cy.get('#logout').should('be.visible');
     cy.get('#logout').click();
   });
 
@@ -41,7 +43,9 @@ describe('Smoke tests', () => {
 
     loginToAuth0();
 
-    cy.get('#logout').should('be.visible');
+    // Make sure the table has rendered with data as that is when the page has loaded completely
+    // and there shouldn't be any issues with the logout button being recreated
+    cy.get('table tbody tr').should('have.length', 2);
     cy.get('table').contains('bob@example.com');
     cy.get('#logout').click();
   });
