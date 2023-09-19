90c81e2a1a2ca09ebc5a20ec043293ccb439719b
frederikprijck <frederik.prijck@auth0.com>
2023-09-19 10:41:50 +0200

    update browserstack and cypress

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  chromeWebSecurity: false,
+  viewportWidth: 1000,
+  viewportHeight: 1000,
+  fixturesFolder: false,
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'test-results/cypress/junit-[hash].xml',
+  },
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost:3000',
+    supportFile: false,
+  },
+})
