578b7e831f37ad0640717e7a0846964290e76a7b
Tushar Pandey <tushar.pandey@okta.com>
2025-01-15 12:41:58 +0530

    update tests

--- __tests__/with-auth0.test.tsx
+++ __tests__/with-auth0.test.tsx
@@ -1,13 +1,16 @@
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
+
 import React, { Component } from 'react';
-import withAuth0, { WithAuth0Props } from '../src/with-auth0';
+
 import { render, screen } from '@testing-library/react';
+
 import { Auth0ContextInterface, initialContext } from '../src/auth0-context';
+import withAuth0, { WithAuth0Props } from '../src/with-auth0';
 
 describe('withAuth0', () => {
   it('should wrap a class component', () => {
     class MyComponent extends Component<WithAuth0Props> {
-      render(): JSX.Element {
+      render()  {
         return <>hasAuth: {`${!!this.props.auth0}`}</>;
       }
     }
@@ -19,7 +22,7 @@ describe('withAuth0', () => {
   it('should wrap a class component and provide context', () => {
     const context = React.createContext<Auth0ContextInterface>(initialContext);
     class MyComponent extends Component<WithAuth0Props> {
-      render(): JSX.Element {
+      render()  {
         return <>hasAuth: {`${!!this.props.auth0}`}</>;
       }
     }
