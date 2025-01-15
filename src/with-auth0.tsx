c5000361ef3390006a78db34da86dff2d82d5c0e
Tushar Pandey <tushar.pandey@okta.com>
2025-01-15 12:40:33 +0530

    add explicit type annotations for React.JSX.Element

--- src/with-auth0.tsx
+++ src/with-auth0.tsx
@@ -30,10 +30,10 @@ const withAuth0 = <P extends WithAuth0Props>(
   Component: ComponentType<P>,
   context = Auth0Context
 ): ComponentType<Omit<P, keyof WithAuth0Props>> => {
-  return function WithAuth(props): JSX.Element {
+  return function WithAuth(props): React.JSX.Element {
     return (
       <context.Consumer>
-        {(auth: Auth0ContextInterface): JSX.Element => (
+        {(auth: Auth0ContextInterface): React.JSX.Element => (
           <Component {...(props as P)} auth0={auth} />
         )}
       </context.Consumer>
