2c8addc985316b5e27255c77dd2f178d9fba83fc
Gyanesh Gouraw <gyanesh.gouraw@okta.com>
2025-07-11 10:12:09 +0530

    fix: standardize variable names for user credentials in smoke tests

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,15 +1,15 @@
-const SMOKE_TEST_EMAIL = Cypress.env('USER_EMAIL');
-const USER_PASSWORD = Cypress.env('USER_PASSWORD');
+const EMAIL = Cypress.env('USER_EMAIL');
+const PASSWORD = Cypress.env('USER_PASSWORD');
 
-if (!SMOKE_TEST_EMAIL || !USER_PASSWORD) {
+if (!EMAIL || !PASSWORD) {
   throw new Error(
     'You must provide CYPRESS_USER_EMAIL and CYPRESS_USER_PASSWORD environment variables'
   );
 }
 
 const loginToAuth0 = (): void => {
-  cy.get('#username').clear().type(SMOKE_TEST_EMAIL);
-  cy.get('#password').clear().type(USER_PASSWORD);
+  cy.get('#username').clear().type(EMAIL);
+  cy.get('#password').clear().type(PASSWORD);
   cy.get('button[type="submit"][name="action"]').contains(/^continue$/i).click({ force: true });
 };
 
@@ -21,7 +21,7 @@ describe('Smoke tests', () => {
 
     loginToAuth0();
 
-    cy.get('#hello').contains(`Hello, ${SMOKE_TEST_EMAIL}!`);
+    cy.get('#hello').contains(`Hello, ${EMAIL}!`);
     cy.get('#logout').click();
     cy.get('#login').should('exist');
   });
