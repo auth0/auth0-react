953ccb1d1739e50922650171d7ec8b1545f0f4c1
Yogesh Chaudhary <yogesh.chaudhary@okta.com>
2026-09-07 15:53:33 +0530

    chore: fix port, TypeScript version, and env type in Vite example

--- examples/cra-react-router/src/react-app-env.d.ts
+++ examples/cra-react-router/src/react-app-env.d.ts
@@ -4,7 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_DOMAIN: string;
   readonly VITE_CLIENT_ID: string;
   readonly VITE_AUDIENCE: string;
-  readonly VITE_API_PORT: number;
+  readonly VITE_API_PORT: string;
 }
 
 interface ImportMeta {
--- examples/cra-react-router/vite.config.ts
+++ examples/cra-react-router/vite.config.ts
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
 });
